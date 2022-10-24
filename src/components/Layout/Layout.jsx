f9c37dea38286ddae1dec36c6f0b5c0a77fb110b
Vitalii Boiko <107396176+vitaliiseeker@users.noreply.github.com>
2022-10-25 00:54:34 +0300

    Update Layout.jsx

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -13,7 +13,7 @@ export const Layout = () => {
         <Nav>
           <List>
             <Item>
-              <LinkNav className="active" to="/" end>Home</LinkNav>
+              <LinkNav to="/" end>Home</LinkNav>
             </Item>
             <Item>
               <LinkNav to="movies">Movies</LinkNav>
